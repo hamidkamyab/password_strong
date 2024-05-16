3cab75c5fe4a28a73a8f7552c04b180c7a12d3a0
Hamid Kamyab <mr_kamyab@yahoo.com>
2024-05-16 23:54:04 +0330

    create the project template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import * as Vsc from "react-icons/vsc";
+import PasswordMeter from "./components/PasswordMeter";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App vh-100 d-flex flex-column justify-content-between container">
+      <div className="main d-flex flex-column align-items-center pt-5">
+        <h4>رمـــــــز ایمــــن</h4>
+
+        <div className="d-flex justify-content-center my-5 col-10">
+          <div className="form-group col-6">
+            <input type="text" className="passInput form-control mb-3" placeholder="رمز مورد نظرتان را وارد کنید..." />
+
+            <PasswordMeter />
+          </div>
+
+        </div>
+      </div>
+
+      <div className="footer text-center py-3">
+        <small> طراحی شده توسط <a href="https://hamidkamyab.ir/" target="_blank" className="text-muted">حمید کامیاب</a></small>
+      </div>
     </div>
   );
 }
---
+++ src/components/PasswordMeter.js
@@ -0,0 +1,17 @@
+﻿import React from 'react'
+
+function PasswordMeter() {
+    return (
+        <div>
+            <div className="progress" role="progressbar" aria-label="Example 1px high" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style={{ height: '3px' }}>
+                <div className="progress-bar" style={{ width: '25%' }}></div>
+            </div>
+            <div className='strongText position-relative mt-2'>
+                <small className='text-success bg-white px-3'>قوی</small>
+            </div>
+
+        </div>
+    )
+}
+
+export default PasswordMeter
