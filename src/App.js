022db9fa252c218f998f7dd222cb237cb325743b
Hamid Kamyab <mr_kamyab@yahoo.com>
2024-05-17 17:52:21 +0330

    Construction of the section for checking and displaying password strength

--- src/App.js
+++ src/App.js
@@ -1,7 +1,70 @@
 import * as Vsc from "react-icons/vsc";
 import PasswordMeter from "./components/PasswordMeter";
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [password,setPassword] = useState("")
+  const [score,setScore] = useState(0)
+
+
+  let upCase = 0;
+  let lwCase = 0;
+  let numCase = 0;
+  let spCase = 0;
+  const checkPass = (val)=>{
+    if(!/^\d+$/.test(val) && !/[^a-zA-Z0-9\-\/]/.test(val) && val === val.toUpperCase()){
+      upCase = 14;
+    }
+    if(!/^\d+$/.test(val) && !/[^a-zA-Z0-9\-\/]/.test(val) && val === val.toLowerCase()){
+      lwCase = 14;
+    }
+    if(/^\d+$/.test(val)){
+      numCase = 14;
+    }
+    if( /[^a-zA-Z0-9\-\/]/.test(val)){
+      spCase = 14;
+    }
+  }
+
+  const handlePass = (val)=>{
+    setPassword(val)
+
+    const arr = val.split('');
+    arr.map((char)=>{
+      checkPass(char)
+    })
+
+    let count = upCase + lwCase + numCase + spCase;
+    let lenChar = -42;
+    if(val.length >= 12){
+      lenChar = 14
+    }else if (val.length >= 8){
+      lenChar = 0
+    }
+
+    
+    if(val.length >= 24 && count <= 42){
+      setScore(3)
+      return;
+    }
+    count = count + lenChar;
+    count == 70?
+      count = count + 10
+    :
+      count = count - 10
+    ;
+
+    if(val.length == 0){
+      setScore(0)
+    }else if(Math.ceil((count / 80) * 4) <= 0){
+      setScore(1)
+    }else{
+      setScore(Math.ceil((count / 80) * 4))
+    }
+    
+  }
+
   return (
     <div className="App vh-100 d-flex flex-column justify-content-between container">
       <div className="main d-flex flex-column align-items-center pt-5">
@@ -9,9 +72,12 @@ function App() {
 
         <div className="d-flex justify-content-center my-5 col-10">
           <div className="form-group col-6">
-            <input type="text" className="passInput form-control mb-3" placeholder="رمز مورد نظرتان را وارد کنید..." />
-
-            <PasswordMeter />
+            <input type="text" 
+            onChange={(e)=>handlePass(e.target.value)}
+            className="passInput form-control mb-3" 
+            placeholder="رمز مورد نظرتان را وارد کنید..." />
+            
+            <PasswordMeter score={score} />
           </div>
 
         </div>
--- src/components/PasswordMeter.js
+++ src/components/PasswordMeter.js
@@ -1,13 +1,64 @@
 ﻿import React from 'react'
 
-function PasswordMeter() {
+function PasswordMeter({ score }) {
+
+    const progressColor = () => ({
+        width: `${score * 25}%`,
+        background: colorCode()
+    })
+
+    const labelText = ()=>{
+        switch (score) {
+            case 1:
+                return 'ضعـــیف';
+                break;
+            case 2:
+                return 'متوســـط';
+                break;
+            case 3:
+                return 'قـــوی';
+                break;
+            case 4:
+                return 'بسیار قـــوی';
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    const colorCode = () => {
+        switch (score) {
+            case 0:
+                return '#a8a8a8';
+                break;
+            case 1:
+                return '#d63031';
+                break;
+            case 2:
+                return '#fa983a';
+                break;
+            case 3:
+                return '#2ed573';
+                break;
+            case 4:
+                return '#008000';
+                break;
+
+            default:
+                break;
+        }
+    }
+
     return (
         <div>
             <div className="progress" role="progressbar" aria-label="Example 1px high" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style={{ height: '3px' }}>
-                <div className="progress-bar" style={{ width: '25%' }}></div>
+                <div className="progress-bar" style={progressColor()}></div>
             </div>
-            <div className='strongText position-relative mt-2'>
-                <small className='text-success bg-white px-3'>قوی</small>
+            <div className={`strongText position-relative mt-2 ${score == 0?'d-none':''}`}>
+                <small className='bg-white px-3' style={{color:`${colorCode()}`}}>
+                    {labelText()}
+                </small>
             </div>
 
         </div>
